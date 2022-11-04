ab5f9fa80dff42ff1adc5b551e97215aa0ce2b65
karl joyeux <karl.joyeux@efrei.net>
2022-11-04 01:47:18 +0100

    Boutique de drapeaux, base card

---
+++ src/components/BaseComponents/BaseCard.vue
@@ -0,0 +1,21 @@
+<template lang="fr" >
+    <img :src ="this.country.flags.png" :alt ="this.country.translations.fr"/>
+    <h4>{{this.country.translations.fr}}</h4>
+    <p>Ce pays situé en {{this.country.region}}</p>
+    <div id = "PetitsBouttons">
+        <button><p>-</p></button>
+        <button><p>+</p></button>
+    </div>
+</template>
+<script>
+export default {
+    name: "BaseCard", 
+    props: {
+        country : Object
+    }
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <BaseCard v-for="(country,i) in countries" :key="i" :country="country"/>
+    
+  </div>
+</template>
+<script>
+import axios from 'axios'
+import BaseCard from '@/components/BaseComponents/BaseCard.vue'
+export default {
+  name :"AboutView",
+  components : {BaseCard},
+  data() {
+    return {
+      countries : Object,
+      url : "https://restcountries.com/v3.1/all"
+    }
+  },
+  created() {
+    axios
+    .get(this.url)
+    .then((response)=>{this.countries = response.data; console.log(this.countries);})
+    .catch(error => console.log(error))
+  }
+}
+</script>
